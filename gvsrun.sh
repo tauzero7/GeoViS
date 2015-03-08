4396b6a60e8fc6adc8872425357cfaa37ebddf97
Thomas <tauzero7@gmail.com>
2015-03-08 15:00:35 +0100

    run script added

---
+++ gvsrun.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+MPIRUN=/usr/local/openmpi/1.8.3/bin/mpirun
+GVS=$HOME/Projekte/GeoViS_M4D/gvsRenderPar64
+
+NUM_PROCS=6
+NUM_TASKS=72
+
+SCM_FILE=""
+IMG_FILE=""
+RENDERDEV=""
+STARTDEV=""
+RENDER_CMD=""
+START_CMD=""
+
+
+while getopts "h?f:i:r:s:t:" opt; do
+	case "$opt" in
+		h|\?)
+			echo "--------------------------------------------"
+			echo "Usage: ./run.sh"
+			echo "   -h/-?    show this help"
+			echo "   -f       scheme filename"
+            echo "   -i       image output filename"
+            echo "   -r <n>   render only image 'n'"
+            echo "   -s <n>   start rendering with image 'n'"
+			echo "--------------------------------------------"
+			exit 0
+			;;
+		f) SCM_FILE=$OPTARG
+			;;
+		i) IMG_FILE=$OPTARG
+			;;
+		r) RENDERDEV=$OPTARG
+			;;
+		s) STARTDEV=$OPTARG
+			;;
+		t) NUM_TASKS=$OPTARG
+			;;
+	esac
+done
+
+
+if [ ! -z $RENDERDEV ] && [ ! -z $STARTDEV ]; then
+	echo "Use either -r or -s"
+	exit
+fi
+
+if [ -z $SCM_FILE ] || [ -z $IMG_FILE ]; then
+	echo "You need at least scm- and img-file"
+	exit
+fi
+
+if [ ! -z $RENDERDEV ]; then 
+	RENDER_CMD="-renderdev $RENDERDEV"
+fi
+
+if [ ! -z $STARTDEV ]; then
+	START_CMD="-startdev $STARTDEV"
+fi
+
+echo "Run: " $SCM_FILE $IMG_FILE " with " $NUM_TASKS " tasks"
+time $MPIRUN -np $NUM_PROCS $GVS -tasks $NUM_TASKS $RENDER_CMD $START_CMD $SCM_FILE $IMG_FILE
+
